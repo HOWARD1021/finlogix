1c6c44f02b4c99b4d1768ea1f38b3a22bd702916
HOWARD1021 <44057435+HOWARD1021@users.noreply.github.com>
2022-08-03 10:36:17 +0800

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar/index'
+import Content from './components/content/index'
+import Cardcontent from './components/cardContent/index'
+import Input from './components/input/index'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbar/>
+     <Content/>
+     <Cardcontent/>
+     <Input/>     
     </div>
+    </Router>
+
   );
 }
-
 export default App;
